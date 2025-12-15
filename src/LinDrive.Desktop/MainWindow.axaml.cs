544fce2ee56fad2b14d5896d868ac236f8fb4ecf
katana <r.grigorev1502@gmail.com>
2025-12-15 19:27:29 +0300

    update projects to dotnet 10

--- src/LinDrive.Desktop/MainWindow.axaml.cs
+++ src/LinDrive.Desktop/MainWindow.axaml.cs
@@ -1,18 +1,18 @@
-using System.IO;
-using Avalonia.Controls;
-using LinDrive.Application.Services.IO.Services;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace LinDrive.Desktop;
-
-public partial class MainWindow : Window
-{
-    private readonly FileWatcherService _watcher;
-    
-    public MainWindow()
-    {
-        _watcher = App.ServiceProvider.GetRequiredService<FileWatcherService>();
-        
-        InitializeComponent();
-    }
-}
+// using System.IO;
+// using Avalonia.Controls;
+// using LinDrive.Application.Services.IO.Services;
+// using Microsoft.Extensions.DependencyInjection;
+//
+// namespace LinDrive.Desktop;
+//
+// public partial class MainWindow : Window
+// {
+//     private readonly FileWatcherService _watcher;
+//     
+//     public MainWindow()
+//     {
+//         _watcher = App.ServiceProvider.GetRequiredService<FileWatcherService>();
+//         
+//         InitializeComponent();
+//     }
+// }
